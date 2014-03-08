e35dd0af56979d5df996d8d552dfaf2e9db9a4ac
Cody Rayment <cody@crayment.com>
2014-03-08 12:12:57 -0700

    Imported user code snippets

---
+++ const.m
@@ -0,0 +1,10 @@
+// const
+// 
+//
+// IDECodeSnippetCompletionPrefix: const
+// IDECodeSnippetCompletionScopes: [TopLevel]
+// IDECodeSnippetIdentifier: 4820439F-7B63-43B7-B60C-FB912E01A74B
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+NSString * const LAMUserBookmarkChangedNotification = @"LAMUserBookmarkChangedNotification";
---
+++ weakify.m
@@ -0,0 +1,10 @@
+// weakify
+// 
+//
+// IDECodeSnippetCompletionPrefix: weak
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 4EC618CD-AB1E-48FE-B70D-A09ABB502413
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+__weak typeof(<#name#>) _<#name#> = <#name#>;
